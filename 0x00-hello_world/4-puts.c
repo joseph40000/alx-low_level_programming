53fbf600746c691ab3ca1036d5e7599187fab2ee
root <joseph40000@github.com>
2021-08-27 06:05:41 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
-/* first programming in c */
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int main()
+/**
+ *main - Main is where my project is being compiled
+ *
+ *
+ * Return: return 0 everytime
+ */
+int main (void)
 {
-        puts("Programming is like building a multilingual puzzle");
-                getchar();
- return 0;
+	char displayed[52];
+	strcpy(displayed, "\"""programming is like building a multilinguall puzzle");
+	puts(displayed);
+	return (0);
 }
