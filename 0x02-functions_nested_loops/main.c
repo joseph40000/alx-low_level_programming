ada691f1457928614fa16b0e675022672022d67d
root <joseph40000@github.com>
2021-09-01 11:55:27 -0700

    task

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+int print_alphabet(void);
+int print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+#endif
