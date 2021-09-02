7363f0ca4b9dbd71756f49630e2f5fae5fcbbb69
root <joseph40000@github.com>
2021-09-02 11:58:35 -0700

    Multiple main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,26 +1,11 @@
-#include "main.h"
-#ifnder MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-/**
-*main - entry point of programs
-*
-*Return: return 0
-*/
-int main(void)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else
-	 printf("%d is zero\n", n);
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
 #endif /* MAIN_H */
-}
