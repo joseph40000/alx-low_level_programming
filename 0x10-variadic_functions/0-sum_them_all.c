e7b74d544dc8052d236dd2573bbfe2493099866b
Joseph Nathan <solomdavid23@gmail.com>
2021-11-19 03:36:34 -0800

    Beauty in variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,13 +1,14 @@
+#include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
 * main - Returns the sum of all its paramters.
-* A variable number of paramters to calculate the sum of.
+*
 * 
 * Return: sum.
 */
-int sum_them_all(const unsigned int n, . . . )
+int sum_them_all(const unsigned int n, ...)
 {
 		va_list nums;
 		unsigned int j;
