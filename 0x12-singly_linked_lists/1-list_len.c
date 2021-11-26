50244e121c78e360338b71f76bb90b09857c8c87
Joseph Nathan <solomdavid23@gmail.com>
2021-11-26 14:15:47 -0800

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len -   elements of linked list_t list.
+ * @h: linked list
+ * 
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+		size_t x_element = 0;
+			const list_t *current = h;
+
+				while (current != NULL)
+						{
+									current = current->next;
+											x_element++;
+												}
+					return (x_element);
+}
