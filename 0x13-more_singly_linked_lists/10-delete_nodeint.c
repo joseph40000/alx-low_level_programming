ddd2830afb3f86c61ff05fd53dfb167bb4a56e5d
Joseph Nathan <solomdavid23@gmail.com>
2021-12-01 07:02:45 -0800

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,36 +1,42 @@
 #include "lists.h"
-#include <stdlib.h>
-
 /**
-* delete_nodeint_at_index - deletes the node at  index
-* of a listint_t linked list.
-* @head: double pointer to the first node in the list
-* @index: index of the node to delete
-*
-* Return: pointer to the indexed node
-*/
+ * delete_nodeint_at_index - Entry Point
+ * @head: head
+ * @index: index
+ * Return: 0
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *current, *next;
-	if (head == NULL || *head == NULL)
-	return (-1);
+	unsigned int count = 0;
+
+	listint_t *copy;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (-1);
+
+	copy = *head;
+
 	if (index == 0)
-		{
-    	next = (*head)->next;
-										free(*head);
-										*head = next;
-										return (1);
-									}
-	current = *head;
-	for (i = 0; i < index - 1; i++)
 	{
-										if (current->next == NULL)
-										return (-1);
-      										current = current->next;
-										}
-		next = current->next;
-		current->next = next->next;
-									free(next);
-									return (1);
+		temp = copy->next;
+		free(copy);
+		*head = temp;
+		return (1);
+	}
+
+	while (copy != NULL)
+	{
+		if (count + 1 == index)
+		{
+			temp = copy->next;
+			copy->next = temp->next;
+			free(temp);
+			return (1);
+		}
+		if (copy->next != NULL)
+			copy = copy->next;
+		count++;
+	}
+	return (-1);
 }
