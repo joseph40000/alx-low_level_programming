7ec6aae5e55c3af692a7b745e60199457a74138f
Joseph Nathan <solomdavid23@gmail.com>
2021-12-07 14:05:38 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,9 +5,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
 /**
- * _strlen - finds the lenghth of a string
+ * _strlen - finds the length of a string
  * @str: pointer to the string
  *
  * Return: length of the string
@@ -16,12 +15,10 @@ size_t _strlen(char *str)
 {
 	size_t i;
 
-	for (i = 0; str[i]; i++0
-			;
-			return (i);
-			
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
 }
-
 
 /**
  * create_file - creates a file.
@@ -32,14 +29,12 @@ size_t _strlen(char *str)
  */
 int create_file(const char *filename, char *text_content)
 {
-
-
-        int fd;
+	int fd;
 	ssize_t len = 0;
 
-	if  (filename == NULL)
+	if (filename == NULL)
 		return (-1);
-	fd =open(filename,0_WRONLY | 0_CREATE | 0_TRUNC, S_IRUSR | S_IWUAR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -47,6 +42,5 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	if (len == -1)
 		return (-1);
-	return (1):
+	return (1);
 }
-
--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -4,20 +4,20 @@
 #include "holberton.h"
 
 /**
- * main - check the code
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-    int res;
+	int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
