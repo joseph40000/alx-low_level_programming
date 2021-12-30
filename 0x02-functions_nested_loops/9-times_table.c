75c541dae2cd41b98b89b5338a0c6c3ab16a44d6
Joseph Nathan <solomdavid23@gmail.com>
2021-12-30 13:45:06 -0800

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * main : times_table - prints the times table
+ *
+ * Return: Always 0 or 1;
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for(i = 0; i < 10; j++)
+	{
+		if (j == 0)
+		{
+			_putchar(48);
+			continue;
+			if(j == 0)
+			{
+
+				_putchar(48);
+				continue;
+			}
+			product = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if(product >= 10)
+			{
+				_putchar(product / 10 +48);
+				_putchar(product % 10 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(product + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
