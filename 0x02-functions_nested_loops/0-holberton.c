5700b763dfca0c23f516ea21b37d092dfa05b9b2
Joseph Nathan <solomdavid23@gmail.com>
2021-12-30 09:33:51 -0800

    Prototype

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,30 @@
-#include "holberton.h"
-/**
- * main - print holberton
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-       _putchar('_');
-       _putchar('p');
-       _putchar('u');
-       _putchar('t');
-       _putchar('c');
-       _putchar('h');
-       _putchar('a');
-       _putchar('r');
-       _putchar('\n')
-       return (0);
-}
+#define HOLBERTON_H
 
+/* _putchar.c */
+int _putchar(char);
+
+/*10 x alphabet */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int n);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
